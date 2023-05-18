7906985a6691a4f631041daf57b83ebddd93bec6
TretyakovaA <putinvivo@bk.ru>
2023-05-18 16:33:53 +0300

    Initial commit

---
+++ src/test/java/pro/sky/maternity/MaternityApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.maternity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaternityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
